cdd6509eede404462c84a8abc9920cfbb1c33a76
Boris Wilhelms <mail@wille-zone.de>
2019-09-23 10:57:36 +0200

    Moves ProductListItem out of partial class

--- backend/BastaProductBackend/BastaProductBackend/Models/ProductListItem.cs
+++ backend/BastaProductBackend/BastaProductBackend/Models/ProductListItem.cs
@@ -2,16 +2,12 @@
 
 namespace BastaProductBackend
 {
-    public static partial class GetAllProducts
+    public class ProductListItem
     {
-        public class ProductListItem
-        {
-            public Guid Id { get; set; }
+        public Guid Id { get; set; }
 
-            public string Manufacturer { get; set; }
+        public string Manufacturer { get; set; }
 
-            public string Name { get; set; }
-        }
-
+        public string Name { get; set; }
     }
 }
