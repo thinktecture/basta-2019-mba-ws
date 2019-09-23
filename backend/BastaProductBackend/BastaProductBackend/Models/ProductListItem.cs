069dc536971cb70b92a4f5b5d63dd6a7829a2b54
Boris Wilhelms <mail@wille-zone.de>
2019-09-23 11:32:07 +0200

    Adds json property

--- backend/BastaProductBackend/BastaProductBackend/Models/ProductListItem.cs
+++ backend/BastaProductBackend/BastaProductBackend/Models/ProductListItem.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace BastaProductBackend
 {
     public class ProductListItem
     {
+        [JsonProperty("id")]
         public Guid Id { get; set; }
 
         public string Manufacturer { get; set; }
